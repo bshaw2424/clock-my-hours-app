1bac12f575f30c03691f2245ff0662b1bd9ab793
Bishop Shaw III <prototype124@yahoo.com>
2025-02-21 19:47:50 -0700

    updated dashboard component styles

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,54 +1,24 @@
-import { useEffect, useState } from "react";
-import { useUser } from "../Context/useUser";
-import axios from "axios";
-
-interface User {
-  username: string;
-}
+import VerticalNav from "./VerticalNav";
+import Calendar from "./Calendar/Calendar";
 
 const Dashboard = () => {
-  const [usernames, setUsernames] = useState<User>({});
-  const { userId } = useUser();
-
-  // const [csrfToken, setCsrfToken] = useState("");
-
-  // useEffect(() => {
-  //   const getCsrfToken = async () => {
-  //     try {
-  //       const response = await axios.get("http://127.0.0.1:8000/token/", {
-  //         withCredentials: true, // ensures cookies (including csrf) are sent
-  //       });
-  //       setCsrfToken(response.data.csrfToken); // Make sure your backend returns the CSRF token as 'csrfToken'
-  //     } catch (error) {
-  //       console.error("Error fetching CSRF Token:", error);
-  //     }
-  //   };
-  //   getCsrfToken();
-  // }, []);
-  console.log(`username ${userId}`);
-  useEffect(() => {
-    const getUserInfo = async () => {
-      try {
-        const response = await axios.get(
-          `http://127.0.0.1:8000/user/${userId}/`,
-          {
-            withCredentials: true,
-          },
-        );
-        console.log(usernames);
-        setUsernames(response.data);
-      } catch (error) {
-        console.log("there was an error " + error);
-      }
-    };
-    getUserInfo();
-  }, []);
-
   return (
     <div style={{ minHeight: "100vh" }} className="container">
-      <h1 className=" mt-5 bg-dark text-white py-3 px-5">
-        Welcome {usernames.username}
-      </h1>
+      <section
+        className=""
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 4fr 2fr",
+          gap: "10px",
+        }}
+      >
+        <VerticalNav />
+        <Calendar />
+        <div className="d-flex flex-column">
+          <div className="border border-1 border-dark h-50">dfjdf</div>
+          <div className="border border-1 border-dark h-50">dfadf</div>
+        </div>
+      </section>
     </div>
   );
 };
