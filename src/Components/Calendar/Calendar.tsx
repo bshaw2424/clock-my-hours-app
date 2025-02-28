f64330a9af45119fba0b1535169a12af8547822f
Bishop Shaw III <prototype124@yahoo.com>
2025-02-27 18:51:37 -0700

    added functionality where the title of the company appears when clicked in the vertical nav

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,24 +1,81 @@
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import EventModal from "../Modal";
 
-const Calendar = () => {
+interface Event {
+  title: string;
+  start: string;
+  end: string;
+}
+
+const Calendar = ({ company_title }) => {
+  const [events, setEvents] = useState<Event[]>([]);
+  // [
+  //   {
+  //     title: "Overnight Shift",
+  //     start: "2025-01-30T23:00:00",
+  //     end: "2025-01-31T07:30:00",
+  //   },
+  //   {
+  //     title: "Lunch Break",
+  //     start: "2025-01-15T12:30:00",
+  //     end: "2025-01-15T13:30:00",
+  //   },
+  //   {
+  //     title: "Afternoon Call",
+  //     start: "2025-01-30T15:00:00",
+  //     end: "2025-01-30T16:00:00",
+  //   },
+  // ];
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [selectedEvents, setSelectedEvents] = useState<Event[]>([]);
+
+  const handleDateSelect = (selectInfo: string) => {
+    const selectedDate = selectInfo.startStr.split("T")[0];
+    setSelectedDate(selectedDate);
+
+    // Filter events based on selected date
+    const eventsForDate = events.filter(
+      event => event.start.split("T")[0] === selectedDate,
+    );
+    setSelectedEvents(eventsForDate);
+  };
+
   return (
-    <section>
-      <div className="my-5" style={{ display: "none" }}>
-        <h1 className="text-center border border-1 border-black mb-3 py-2">
-          Work Schedule
-        </h1>
+    <section className="mt-5 p-0">
+      <h1 className="text-center my-3 bg-grey">{company_title}</h1>
+      <div className="m-3">
+        <div className="mb-3">
+          <Link to="/newShift" className="btn btn-primary">
+            Add Shift
+          </Link>
+        </div>
+
+        {/* FullCalendar Component */}
         <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin]}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           headerToolbar={{
-            start: "today prev, next",
+            start: "today prev,next",
             center: "title",
             end: "dayGridMonth,timeGridWeek,timeGridDay",
           }}
-          height={650}
+          selectable={true}
+          select={handleDateSelect}
+          height={600}
+          events={events}
         />
+        {/* Render Event Modal Only When a Date is Selected */}
+        {selectedDate && (
+          <EventModal
+            selectedEvents={selectedEvents}
+            selectedDate={selectedDate}
+          />
+        )}
       </div>
     </section>
   );
