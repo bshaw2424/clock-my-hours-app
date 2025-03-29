ef2f1b3a6b8d5f0e40af12813b7e59642391a7bc
Bishop Shaw III <prototype124@yahoo.com>
2025-03-28 19:04:22 -0700

    deleted the FullCalendar design and created my own

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,105 +1,20 @@
-import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import { useState } from "react";
-import EventModal from "../Modal";
-import NewShift from "../Forms/NewShift";
+import MonthYearDisplay from "./MonthYearDisplay";
 // import { useCompany } from "../../Context/CompanyContext";
-
-// interface Event {
-//   title: string;
-//   start: string;
-//   end: string;
-// }
 
 interface CompanyTitle {
   company_title: string;
   events: [];
+  payPeriods: [];
 }
 
-const Calendar = ({ company_title, events }: CompanyTitle) => {
-  // const { companyData, selectedCompany, setSelectedCompany } = useCompany();
-
-  // const event = [
-  //   {
-  //     title: "Overnight Shift",
-  //     start: "2025-01-30T23:00:00",
-  //     end: "2025-01-31T07:30:00",
-  //   },
-  //   {
-  //     title: "Lunch Break",
-  //     start: "2025-01-15T12:30:00",
-  //     end: "2025-01-15T13:30:00",
-  //   },
-  //   {
-  //     title: "Afternoon Call",
-  //     start: "2025-01-30T15:00:00",
-  //     end: "2025-01-30T16:00:00",
-  //   },
-  //   {
-  //     title: "Work On Coding",
-  //     start: "2025-03-12T15:00:00",
-  //     end: "2025-03-12T16:00:00",
-  //   },
-  // ];
-
-  // const [events, setEvents] = useState<Event[]>([]);
-  const [selectedDate, setSelectedDate] = useState<string | null>(null);
-  const [selectedEvents, setSelectedEvents] = useState<Event[]>([]);
-  const [showForm, setShowForm] = useState(false);
-
-  const handleDateSelect = (selectInfo: string) => {
-    const selectedDate = selectInfo.startStr.split("T")[0];
-    setSelectedDate(selectedDate);
-
-    // Filter events based on selected date
-    const eventsForDate = events.filter(
-      event => event.start.split("T")[0] === selectedDate,
-    );
-    setSelectedEvents(eventsForDate);
-  };
-
+const Calendar = ({ company_title }: CompanyTitle) => {
   return (
     <section className="mt-5 p-0">
-      <div className="m-3">
-        {!showForm ? (
-          <div>
-            <div className="mb-5 d-flex">
-              <button
-                onClick={() => setShowForm(!showForm)}
-                className="btn btn-primary w-25"
-              >
-                Add Shift
-              </button>
-              <h1 className="bg-grey w-100 text-center p-0">{company_title}</h1>
-            </div>
-
-            <FullCalendar
-              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-              initialView="dayGridMonth"
-              headerToolbar={{
-                start: "today prev,next",
-                center: "title",
-                end: "dayGridMonth,timeGridWeek,timeGridDay",
-              }}
-              selectable={true}
-              select={handleDateSelect}
-              height={600}
-              events={events}
-            />
-          </div>
-        ) : (
-          <NewShift title={company_title} showFormState={setShowForm} />
-        )}
-
-        {/* Render Event Modal Only When a Date is Selected */}
-        {selectedDate && (
-          <EventModal
-            selectedEvents={selectedEvents}
-            selectedDate={selectedDate}
-          />
-        )}
+      <h1 className="bg-grey w-100 p-0 text-center">
+        {company_title.toUpperCase()}
+      </h1>
+      <div className="border border-1 border-dark">
+        <MonthYearDisplay />
       </div>
     </section>
   );
