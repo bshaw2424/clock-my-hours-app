5504e438b95a7059c12406190107c424c9800823
Bishop Shaw III <prototype124@yahoo.com>
2025-04-15 23:09:12 -0700

    updated company create

--- src/Components/Company/Update.tsx
+++ src/Components/Company/Update.tsx
@@ -1,51 +1,37 @@
+import UpdateForm from "./UpdateForm";
+import { useState } from "react";
+import TimeOffUpdate from "./TimeOffUpdate";
+
 const CompaniesUpdate = () => {
+  const [activeForm, setActiveForm] = useState({
+    title: "details",
+    btnColor: "blue",
+  });
+
   return (
-    <div>
-      <h1>Update Company</h1>
-      <form action="" method="post">
-        <div className="d-flex flex-column">
-          <input
-            className="form-control"
-            type="text"
-            name="name"
-            value={"name"}
-            id="name"
-            placeholder="Company Name"
-          />
-          <input
-            className="form-control"
-            type="time"
-            name="start"
-            value={"13:00"}
-            id="startTime"
-          />
-          <input
-            className="form-control"
-            type="time"
-            name="end"
-            value={"21:00"}
-            id="endTime"
-          />
-          <input
-            className="form-control"
-            type="number"
-            name="pay"
-            id="payRate"
-            value={28.25}
-          />
-          <input
-            className="form-control"
-            type="date"
-            name="startDate"
-            id="startDate"
-            value={"02 / 23 / 2024"}
-          />
-        </div>
-        <div className="button-container">
-          <button className="btn btn-primary w-100">Submit</button>
-        </div>
-      </form>
-    </div>
+    <section className="container">
+      <div className="my-4">
+        <button
+          onClick={() => setActiveForm({ title: "details", btnColor: "dark" })}
+          className={`btn me-2 ${
+            activeForm.title === "details" ? "btn-dark" : "btn-primary"
+          }`}
+        >
+          Basic
+        </button>
+        <button
+          onClick={() => setActiveForm({ title: "timeoff", btnColor: "dark" })}
+          className={`btn  ${
+            activeForm.title === "timeoff" ? "btn-dark" : "btn-primary"
+          }`}
+        >
+          Time Off
+        </button>
+      </div>
+
+      {activeForm.title === "details" && <UpdateForm />}
+      {activeForm.title === "timeoff" && <TimeOffUpdate />}
+    </section>
   );
 };
 
